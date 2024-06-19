399d570c1d4f2d68f1fb3df66f180f8d060c51a9
Fanniaaziza <135006271+Fanniaaziza@users.noreply.github.com>
2024-06-19 08:58:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,188 +1,75 @@
-import mysql.connector
+import pymysql
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 from sqlalchemy import create_engine
-from sqlalchemy.exc import OperationalError
 
-# Fungsi untuk membuat koneksi ke database MySQL menggunakan mysql.connector
-def create_mysql_connector_connection():
-    try:
-        conn = mysql.connector.connect(
-            host="kubela.id",
-            port=3306,
-            user="davis2024irwan",
-            password="wh451n9m@ch1n3",
-            database="aw"
-        )
-        if conn.is_connected():
-            print('Connected to MySQL database using mysql.connector')
-            return conn
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-        st.error(f"Error: {err}")
-        return None
+# Membuat engine SQLAlchemy untuk koneksi ke MySQL
+engine = create_engine("mysql+pymysql://davis2024irwan:wh451n9m@ch1n3@kubela.id:3306/aw")
 
-# Fungsi untuk membuat koneksi ke database MySQL menggunakan SQLAlchemy
-def create_sqlalchemy_engine():
-    try:
-        engine = create_engine("mysql+pymysql://davis2024irwan:wh451n9m@ch1n3@kubela.id:3306/aw")
-        conn = engine.connect()
-        print('Connected to MySQL database using SQLAlchemy')
-        return engine
-    except OperationalError as err:
-        print(f"Error: {err}")
-        st.error(f"Error: {err}")
-        return None
+# Koneksi ke database
+conn = pymysql.connect(
+    host="kubela.id",
+    port=3306,
+    user="davis2024irwan",
+    password="wh451n9m@ch1n3",
+    database="aw"
+)
 
-# Membuat koneksi ke database
-conn = create_mysql_connector_connection()
-engine = create_sqlalchemy_engine()
+# Cek koneksi berhasil
+if connection.is_connected():
+    print('Connected to MySQL database')
 
-if conn and engine:
-    # Query SQL untuk mengambil data penjualan per tahun
-    query_sales_yearly = """
-        SELECT CalendarYear AS Year, SUM(factfinance.Amount) AS TotalSales
-        FROM dimtime
-        JOIN factfinance ON dimtime.TimeKey = factfinance.TimeKey
-        GROUP BY CalendarYear
-        ORDER BY CalendarYear
-    """
+# Membuat cursor
+cursor = connection.cursor()
 
-    # Menjalankan query dan membuat DataFrame dari hasilnya
-    df_sales = pd.read_sql(query_sales_yearly, conn)
+# Query SQL untuk mengambil data penjualan per tahun
+query = """
+    SELECT CalendarYear AS Year, SUM(factfinance.Amount) AS TotalSales
+    FROM dimtime
+    JOIN factfinance ON dimtime.TimeKey = factfinance.TimeKey
+    GROUP BY CalendarYear
+    ORDER BY CalendarYear
+"""
 
-    # Menutup koneksi setelah selesai digunakan
-    conn.close()
+# Eksekusi query
+cursor.execute(query)
+data = cursor.fetchall()
 
-    # Konversi kolom 'Year' ke tipe data integer
-    df_sales['Year'] = df_sales['Year'].astype(int)
+# Menutup cursor dan koneksi database
+cursor.close()
+connection.close()
 
-    # Menampilkan judul dashboard
-    st.markdown("<h1 style='text-align: center; color: black;'>Dashboard Adventure Works</h1>", unsafe_allow_html=True)
+# Menampilkan judul dashboard
+st.markdown("<h1 style='text-align: center; color: black;'>Dashboard Adventure Works</h1>", unsafe_allow_html=True)
 
-    # Menampilkan DataFrame di Streamlit dalam bentuk tabel
-    st.subheader('1. Data Penjualan Tahunan')
-    st.dataframe(df_sales)
+# Membuat DataFrame dari hasil query
+df_sales = pd.DataFrame(data, columns=['Year', 'TotalSales'])
 
-    # Rentang tahun yang tersedia
-    tahun_options = range(df_sales['Year'].min(), df_sales['Year'].max() + 1)
+# Konversi kolom 'Year' ke tipe data integer
+df_sales['Year'] = df_sales['Year'].astype(int)
 
-    # Pilihan untuk memilih rentang tahun menggunakan slider
-    year_range = st.slider('Pilih Rentang Tahun:', min_value=min(tahun_options), max_value=max(tahun_options), value=(min(tahun_options), max(tahun_options)), step=1)
+# Menampilkan DataFrame di Streamlit dalam bentuk tabel
+st.subheader('1. Data Penjualan Tahunan')
+st.dataframe(df_sales)
 
-    # Filter data berdasarkan rentang tahun yang dipilih
-    df_filtered = df_sales[(df_sales['Year'] >= year_range[0]) & (df_sales['Year'] <= year_range[1])]
+# Rentang tahun yang tersedia
+tahun_options = range(df_sales['Year'].min(), df_sales['Year'].max() + 1)
 
-    # Plot perbandingan total penjualan per tahun dengan Matplotlib
-    plt.figure(figsize=(12, 6))
-    plt.plot(df_filtered['Year'], df_filtered['TotalSales'], marker='o', linestyle='-', color='b', linewidth=2, markersize=8)
-    plt.title(f'Perbandingan Total Penjualan Tahun {year_range[0]}-{year_range[1]}', fontsize=16)
-    plt.xlabel('Tahun', fontsize=14)
-    plt.ylabel('Total Penjualan', fontsize=14)
-    plt.grid(True)
+# Pilihan untuk memilih rentang tahun menggunakan slider
+year_range = st.slider('Pilih Rentang Tahun:', min_value=min(tahun_options), max_value=max(tahun_options), value=(min(tahun_options), max(tahun_options)), step=1)
 
-    # Menampilkan plot di Streamlit
-    st.markdown(f"<h2 style='text-align: center;'>Grafik Total Penjualan </h2>", unsafe_allow_html=True)
-    st.pyplot(plt)
+# Filter data berdasarkan rentang tahun yang dipilih
+df_filtered = df_sales[(df_sales['Year'] >= year_range[0]) & (df_sales['Year'] <= year_range[1])]
 
-    # Query data untuk bubble plot
-    query_bubble = '''
-    SELECT 
-      st.SalesTerritoryRegion AS Country,
-      SUM(fs.SalesAmount) AS TotalSales  
-    FROM factinternetsales fs
-    JOIN dimsalesterritory st
-      ON fs.SalesTerritoryKey = st.SalesTerritoryKey
-    GROUP BY Country
-    '''
+# Plot perbandingan total penjualan per tahun dengan Matplotlib
+plt.figure(figsize=(12, 6))
+plt.plot(df_filtered['Year'], df_filtered['TotalSales'], marker='o', linestyle='-', color='b', linewidth=2, markersize=8)
+plt.title(f'Perbandingan Total Penjualan Tahun {year_range[0]}-{year_range[1]}', fontsize=16)
+plt.xlabel('Tahun', fontsize=14)
+plt.ylabel('Total Penjualan', fontsize=14)
+plt.grid(True)
 
-    # Membuat DataFrame dari hasil query
-    df_bubble = pd.read_sql(query_bubble, engine)
-
-    # Buat bubble plot
-    plt.figure(figsize=(14, 12))
-    plt.scatter(x=df_bubble['Country'], 
-                y=df_bubble['TotalSales'],
-                s=df_bubble['TotalSales'] / 1000,  # Ukuran bubble diatur lebih kecil untuk visibilitas yang lebih baik
-                c='b',
-                alpha=0.6,  # Menambahkan transparansi untuk visibilitas yang lebih baik
-                edgecolors='w',  # Menambahkan border putih pada bubble
-                linewidth=0.5)
-
-    # Menambahkan label untuk sumbu x dan y serta judul plot
-    plt.xlabel('Country')
-    plt.ylabel('Total Sales')  
-    plt.title('Bubble Plot Hubungan Wilayah dan Penjualan')
-    plt.grid(True)
-
-    # Menampilkan plot di Streamlit
-    st.markdown("<h2 style='text-align: center;'>2. Bubble Plot Hubungan Wilayah dan Penjualan</h2>", unsafe_allow_html=True)
-    st.pyplot(plt)
-
-    # Query data untuk pie chart
-    query_pie = '''
-    SELECT
-        st.SalesTerritoryRegion,
-        SUM(fs.SalesAmount) AS TotalSales
-    FROM
-        factinternetsales fs
-    JOIN
-        dimsalesterritory st ON fs.SalesTerritoryKey = st.SalesTerritoryKey
-    GROUP BY
-        st.SalesTerritoryRegion
-    '''
-
-    # Jalankan query dan simpan hasilnya ke dalam DataFrame
-    df_sales_by_region = pd.read_sql(query_pie, engine)
-
-    # Buat visualisasi proporsi penjualan per wilayah atau region
-    plt.figure(figsize=(10, 6))
-    plt.pie(df_sales_by_region['TotalSales'], labels=df_sales_by_region['SalesTerritoryRegion'], autopct='%1.1f%%', startangle=140)
-    plt.title('Proporsi Penjualan per Wilayah atau Region')
-    plt.axis('equal')  # Membuat pie chart menjadi lingkaran
-
-    # Menampilkan plot di Streamlit
-    st.markdown("<h2 style='text-align: center;'>3. Proporsi Penjualan per Wilayah atau Region</h2>", unsafe_allow_html=True)
-    st.pyplot(plt)
-
-    # Query data untuk bar plot
-    query_bar = '''
-    SELECT
-        pc.EnglishProductCategoryName AS ProductCategory,
-        SUM(fs.SalesAmount) AS TotalSales
-    FROM
-        factinternetsales fs
-    JOIN
-        dimproduct p ON fs.ProductKey = p.ProductKey
-    JOIN
-        dimproductsubcategory psc ON p.ProductSubcategoryKey = psc.ProductSubcategoryKey
-    JOIN
-        dimproductcategory pc ON psc.ProductCategoryKey = pc.ProductCategoryKey
-    GROUP BY
-        pc.EnglishProductCategoryName
-    '''
-
-    # Baca ke DataFrame
-    df = pd.read_sql(query_bar, engine)
-
-    # Buat figure dan axes
-    fig, ax = plt.subplots(figsize=(10, 6))
-
-    # Plot bar (lebih cocok daripada histogram untuk kategori)
-    ax.bar(df['ProductCategory'], df['TotalSales'], color='blue')
-
-    # Setting label        
-    ax.set(title='Komposisi Penjualan per Kategori Produk',
-           ylabel='Total Penjualan',   
-           xlabel='Kategori Produk')
-
-    # Rotasi label x untuk lebih baik
-    plt.xticks(rotation=45)
-
-    # Menampilkan plot di Streamlit
-    st.markdown("<h2 style='text-align: center;'>4. Komposisi Penjualan per Kategori Produk</h2>", unsafe_allow_html=True)
-    st.pyplot(fig)
-
-else:
-    st.error("Failed to connect to the database. Please check your connection settings.")
+# Menampilkan plot di Streamlit
+st.markdown(f"<h2 style='text-align: center;'>Grafik Total Penjualan</h2>", unsafe_allow_html=True)
+st.pyplot(plt)
